a8169594d1a275563b3f71c4cbb34af05e74105a
J_D_Nye <garwoodjdm@gmail.com>
2023-08-11 10:16:17 +1000

    schema not working for some reason

--- db/schema.sql
+++ db/schema.sql
@@ -1,37 +1,24 @@
-DROP DATABASE IF EXISTS Employees_db;
-CREATE DATABASE Employees_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE Employees_db;
+USE employees_db;
 
 CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    department_id INT
 );
 
 CREATE TABLE employee (
-    id INT NOT NUll AUTO_INCREMENT,
+    id INT NOT NUll AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    /*
-    FOREIGN KEY (manager_id)
-    REFERENCES employee(id)
-    */
-    ON DELETE SET NULL
+    manager_id INT
 );
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('dotenv').config();
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password:process.env.DB_PASSWORD,
+        database: 'employees_db'
+        
+    })
 
 inquirer
 .prompt([
@@ -14,38 +22,79 @@ inquirer
     }
 ])
 .then((res) => {
-    const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password:process.env.DB_PASSWORD,
-        database: 'Employees_db'
-        
-    })
-    return res.action
+    return res.action;
 })
 .then((act) => {
     switch(act) {
         case 'view all departments':
             console.log(act);
+            db.query(`SELECT * FROM department`, (err, result) => {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(result);
+              });
             break;
         case 'view all roles':
             console.log(act);
+            db.query(`SELECT * FROM role`, (err, result) => {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(result);
+              });
             break;
         case 'view all employees':
             console.log(act);
+            db.query(`SELECT * FROM employee`, (err, result) => {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(result);
+              });
             break;
         case 'add a department':
+            console.log(act);
+            inquirer
+            .prompt([
+
+            ])
             console.log(act);
             break;
         case 'add a role':
             console.log(act);
+            inquirer
+            .prompt([
+
+            ])
             break;
         case 'add an employee':
             console.log(act);
+            inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'first_name',
+                    message: 'Enter the employees first name:'
+                },
+                {
+                    type: 'input',
+                    name: 'last_name',
+                    message: 'Enter the employees last name:'
+                },
+                {
+                    type: 'input',
+                    name: 'role_id',
+                    message: 'Enter the employees role id:'
+                }
+            ])
             break;
         case 'update an employee role':
             console.log(act);
+            inquirer
+            .prompt([
+
+            ])
             break;
     }
-})
+});
