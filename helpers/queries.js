143ad2736943e6e7cf66bcd3d0a1a9a3d52e9c3a
J_D_Nye <garwoodjdm@gmail.com>
2023-08-16 01:04:43 +1000

    finished for the night, added updated funct

--- helpers/queries.js
+++ helpers/queries.js
@@ -1,40 +1,4 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
-require('dotenv').config();
-//let i = 1;
-//let j = 0;
-let listofids = []
-let current_emp = [];
-const { update_employee, view_depart, view_roles, view_employees, add_depart, add_role, add_employee } = require('./helpers/queries.js');
-const { sort_input, sort_input_2 } = require("./helpers/sorters.js");
-
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password:process.env.DB_PASSWORD,
-        database: 'employees_db'
-        
-    })
-/*
-const sort_input_2 = (input) => {
-        let column = input.col;
-        switch(column) {
-            case 'first name':
-                column = "first_name";
-                break;
-            case 'last name':
-                column = "last_name";
-                break;
-            case 'role id':
-                column = "role_id";
-                break;
-            case 'manager id':
-                column = "manager_id";
-                break;
-            }
-        return [input.employee_id.toString(), column, input.new_val];
-        }
+const { sort_input, sort_input_2 } = require("./sorters.js");
 
 const update_employee = async () => {
     //db query and loop to extract ids
@@ -76,16 +40,15 @@ const update_employee = async () => {
             });
         })
       })
-      }
-
+}
 
 const view_depart = () => {
-    db.query(` SELECT department.id AS ID, department.department_name AS department FROM department`, (err, result) => {
-        if (err) {
-          console.log(err);
-        }
-        console.log(result);
-      });
+db.query(` SELECT department.id AS ID, department.department_name AS department FROM department`, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+    });
 }
 
 const view_roles = () => {
@@ -96,7 +59,7 @@ const view_roles = () => {
         console.log(result);
         });
 }
-
+    
 const view_employees = () => {
     db.query(`SELECT employee.id AS ID, employee.first_name AS name, employee.last_name AS surname, role.title AS title, role.salary AS salary FROM employee LEFT JOIN role ON employee.role_id = role.id`, (err, result) => {
         if (err) {
@@ -185,91 +148,4 @@ const add_employee = () => {
     })
 }
 
-
-const sort_input = (input) => {
-    act = input.action;
-    switch(act) {
-        case 'view all departments':
-            view_depart();
-            break;
-        case 'view all roles':
-            view_roles()
-            break;
-        case 'view all employees':
-            view_employees()
-            break;
-        case 'add a department':
-            add_depart()
-            break;
-        case 'add a role':
-            add_role()
-            break;
-        case 'add an employee':
-            add_employee()
-            break;
-        case 'update an employee role':
-            update_employee()
-            break;
-        }
-        //ADD DEFAULT 
-    }
-*/
-const get_input = async () => {
-    await inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'action',
-            message: 'What would you like to do?',
-            choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
-        }
-    ]).then((res) => sort_input(res)).then();
-}
-
-//function loopedCLapp
-/*
-const main = () => {
-    i=10
-    j=0
-    while (true) {
-        if(i>j) {
-            j+=1;
-            get_input()
-        }
-    }
-}*/
-
-/*
-const main = async () => {
-    for (let count = 0; count < 3; count++) {
-      await cmdLineLoop()
-  };
-} 
-main()*/
-async function main() {
-    for (j =0; j<10; j+=1) {
-        await get_input()
-    }
-}
-main();
-/*
-const loopedPromiseChain = (iterations, maxIterations) => {
-    if (iterations <= maxIterations) {
-        return cmdLineLoop()
-            .then(() => {
-                return loopedPromiseChain(iterations + 1, maxIterations);
-            });
-    } else {
-        return Promise.resolve();
-    }
-}
-
-const maxIterations = 50;
-
-loopedPromiseChain(1, maxIterations)
-    .then(() => {
-        console.log("loop finito!");
-    })
-    .catch((error) => {
-        console.error("Error: ", error);
-    }*/
+module.exports = {update_employee, view_depart, view_roles, view_employees, add_depart, add_role, add_employee};
